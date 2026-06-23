55adc3b05b4d4e55bababb486c1c0ab49eebe4f6
Sushmita <sushmita.sudhir-tankkar@capgemini.com>
2026-06-23 08:55:58 +0530

    Added TC for Multi Page automation

--- tests/FirstTest.spec.js
+++ tests/FirstTest.spec.js
@@ -10,7 +10,7 @@ test('My first playwright test',async ({page})=>{
 })
 
 
-test.only('Basic UI Tests', async ({page})=>{
+test('Basic UI Tests', async ({page})=>{
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
     console.log(await page.title())
     //console.log(title);
@@ -18,6 +18,7 @@ test.only('Basic UI Tests', async ({page})=>{
     const username =page.locator('#username')
     const password =page.locator('#password')
     const signin =page.locator("#signInBtn")
+    const user=page.getByLabel('User', { exact: true })
     await username.fill('sushtankkar')
     await password.fill('12345')
     await signin.click();
@@ -30,9 +31,43 @@ test.only('Basic UI Tests', async ({page})=>{
     await expect (page.locator("[style*='block']")).toContainText('Old password "learning" is no longer valid.');
     await username.fill('rahulshettyacademy')
     await password.fill('Learning@830$3mK2')
+    await user.check();
+    await expect(user).toBeChecked();
+    await page.locator("#okayBtn").click();
+    await page.locator('select.form-control').selectOption('consult');
+    await page.locator('#terms').check();
+    await expect(page.locator('#terms')).toBeChecked();
+    await expect (page.getByRole('link', { name: 'Free Access to InterviewQues/ResumeAssistance/Material' })).toHaveAttribute('class','blinkingText');
     await signin.click();
     await page.getByText('iphone X').click();
     await page.waitForTimeout(6000);
+
+})
+
+test.only('Multi page automation flow',async({browser})=>{
+    const context= await browser.newContext();
+    const page = await context.newPage();
+    const username =page.locator('#username')
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    const documentlink= await page.locator("[href*='documents-request']")
+    const [page2]= await Promise.all(
+        [
+            context.waitForEvent('page'),
+            documentlink.click(),
+        ]
+    )
+
+    const text=await page2.locator('.red').textContent();
+    //console.log(text);
+
+    const arrText= text.split('@')
+    //console.log(arrText);
+
+    const domainName = arrText[1].split(" ")[0];
+    //console.log(domainName);
+
+    await username.fill(domainName);
+    await page.waitForTimeout(5000);
 
 })
     
